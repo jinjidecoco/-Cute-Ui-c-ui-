80724c82ae84587b9f6e505380abf50f7b5b35a0
jinjidecoco <18895627215@163.com>
2018-03-29 14:43:43 +0800

    第二次更新相关

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,20 @@
-var gulp = require('gulp'),
-    less = require('gulp-less'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    connect = require('gulp-connect'),
-    watch = require('gulp-watch'),
-    sourcemaps = require('gulp-sourcemaps'),
-    queue = require('gulp-sequence'), //让任务安装设置的先后顺序执行 防止相互依赖的问题产生   
-    proxyMiddleware = require('http-proxy-middleware'),
-    lessAll = 'static/less/main.less';
-
+var gulp = require("gulp"),
+    less = require("gulp-less"),
+    concat = require("gulp-concat"),
+    uglify = require("gulp-uglify"),
+    connect = require("gulp-connect"),
+    change = require("gulp-changed"),
+    watch = require("gulp-watch"),
+    sourcemaps = require("gulp-sourcemaps"),
+    queue = require("gulp-sequence"), //让任务安装设置的先后顺序执行 防止相互依赖的问题产生
+    proxyMiddleware = require("http-proxy-middleware"),
+    lessAll = "static/less/main.less";
 
 
 gulp.task('less', function() {
-    gulp
-        .src("lessAll")
+    return gulp
+        .src(lessAll)
+        .pipe(change("./static/css"))
         .pipe(less())
         .pipe(gulp.dest("./static/css"));
 })
@@ -29,6 +30,23 @@ gulp.task('watch', function() {
         gulp.src("./").pipe(connect.reload());
     });
 })
+gulp.task('server', ['watch'], function() {
+    var middleware = proxyMiddleware(["/api"], {
+        target: "https://api.www.hzzwxk.com",
+        changeOrigin: true,
+        pathRewrite: {
+            "^/api": "/"
+        }
+    });
+    connect.server({
+        root: './',
+        port: 8082,
+        livereload: true,
+        middleware: function(connect, opt) {
+            return [middleware];
+        }
+    })
+})
 gulp.task("dev", function(cb) {
-    queue("less", "connect", "watch")(cb);
-});
+    queue("less", "server", "watch")(cb);
+})
---
+++ static/js/index.js
@@ -0,0 +1,8 @@
+function sum(a, b) {
+    console.log(a + b)
+}
+sum(1, 2);
+setTimeout(() => {
+    alert(1);
+
+}, timeout);
