bd1365246c01f0b98a08467271c81bbfc3e96cb6
jinjidecoco <18895627215@163.com>
2018-04-02 11:40:48 +0800

    coco

--- .build/js/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function sum(a, b) {
-    console.log(a + b);
-}
-sum(1, 2);
-setTimeout(function () {
-    alert(1);
-}, timeout);
--- gulpfile.js
+++ gulpfile.js
@@ -7,35 +7,37 @@ var gulp = require("gulp"),
     watch = require("gulp-watch"),
     babel = require("gulp-babel"),
     sourcemaps = require("gulp-sourcemaps"),
-    queue = require("gulp-sequence"), //让任务安装设置的先后顺序执行 防止相互依赖的问题产生
+    queue = require("gulp-sequence"), // 让任务安装设置的先后顺序执行 防止相互依赖的问题产生
     proxyMiddleware = require("http-proxy-middleware"),
-    lessAll = ["static/less/main.less"];
-
+    livereload = require("gulp-livereload"),
+    lessAll = ["./static/less/cute.less"],
+    css = "./build/css";
 
 gulp.task('less', function() {
     return gulp.src(lessAll)
-        .pipe(change("./build/css"))
+        .pipe(change(css))
         .pipe(less())
-        .pipe(gulp.dest("./build/css"));
-});
-gulp.task('watch', function() {
-    var less_watch = lessAll.concat('./static/less/**/*');
+        .pipe(gulp.dest(css))
+})
+gulp.task('watch', function(event) {
+    var less_watch = lessAll.concat('./static/less/*.less')
     gulp.watch(less_watch, function() {
         queue('less')(function() {
-            gulp.src('./')
-                .pipe(connect.reload());
-        });
-    });
-    gulp.watch("./*.html", function() {
-        gulp.src("./").pipe(connect.reload());
-    });
+            gulp.src('./').pipe(connect.reload())
+        })
+        gulp.watch('./*.html', function() {
+            gulp.src('./').pipe(connect.reload())
+        })
+    })
 })
 gulp.task('default', function() {
     return gulp.src('static/js/*.js')
         .pipe(babel())
-        .pipe(gulp.dest('build/js'));
-});
-gulp.task('server', function() {
+        .pipe(gulp.dest('build/js'))
+})
+
+
+gulp.task("servers", function() {
     var middleware = proxyMiddleware(["/api"], {
         target: "https://api.www.hzzwxk.com",
         changeOrigin: true,
@@ -44,14 +46,17 @@ gulp.task('server', function() {
         }
     });
     connect.server({
-        root: './',
-        port: 8082,
+        port: 8091,
+        root: "./",
         livereload: true,
         middleware: function(connect, opt) {
             return [middleware];
         }
-    })
+    });
+});
+
+gulp.task('dev', function(cb) {
+    queue('default', 'less', 'servers', 'watch')(cb)
 })
-gulp.task("dev", function(cb) {
-    queue("default", "less", "server", "watch")(cb);
-})
+
+// gulp.task("dev", ["default", "less", "server", "watch"]);s
